c5cb67de7d25203b5e44f27ee853b2056efe4a04
Nick Russo <nrusso1993@gmail.com>
2021-06-22 17:18:00 -0400

    basic functionality

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+let numUsers = 0;
 
 app.get('/',function(req,res){
    res.sendfile('index.html');
+});
+
+io.on('connection',function(socket){
+   numUsers++;
+   console.log('user connected, numsers: ' + numUsers);
+
+   socket.on('disconnect',function(){
+      numUsers--;
+      
+      console.log('user disconnected, numsers: ' + numUsers);
+   })
 });
 
 http.listen(3000,function(){
